aad5504a836ac69a401c4f9f87c66b4fb02a5d09
chanmob <sellingout@naver.com>
2021-02-13 17:52:42 +0900

    버그 수정

--- DiceDefense/Assets/Scripts/Singleton/InGameManager.cs
+++ DiceDefense/Assets/Scripts/Singleton/InGameManager.cs
@@ -77,6 +77,9 @@ public class InGameManager : Singleton<InGameManager>
             }
         }
 
+        if (enemy.gameObject.activeSelf == false)
+            return null;
+
         return enemy;
     }
 
